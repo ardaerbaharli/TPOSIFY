509a373b1b942e6a4a9f1663a2eafe867fdfcb79
ayberkderingoz <ayberkderingoz@gmail.com>
2022-05-06 15:48:57 +0300

    page controllers

---
+++ Assets/Scripts/Controllers/ArtistPanelController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Controllers
+{
+    public class ArtistPanelController : MonoBehaviour
+    {
+        public void BackButton()
+        {
+            PageController.Instance.GoBack();
+        }
+    }
+}
--- Assets/Scripts/Controllers/CreatePlaylistController.cs
+++ Assets/Scripts/Controllers/CreatePlaylistController.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Enums;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Controllers
 {
     public class CreatePlaylistController : MonoBehaviour
     {
+
+        [SerializeField] private InputField text;
         public void CancelButtonClicked()
         {
             PageController.Instance.GoBack();
@@ -15,6 +18,7 @@ namespace Controllers
         public void CreateButtonClicked()
         {
             PageController.Instance.ShowPage(Pages.Playlist);
+            text.text = "";
         }
         
     }
